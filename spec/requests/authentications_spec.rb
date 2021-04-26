66e5fc443341510d0353c44c37c774734627e8ea
ezeilo-su <ezeilo.su@gmail.com>
2021-04-26 14:59:28 +0100

    Create User model. Add authentication token service.

--- spec/requests/authentications_spec.rb
+++ spec/requests/authentications_spec.rb
@@ -2,8 +2,9 @@ require 'rails_helper'
 
 RSpec.describe 'Authentications', type: :request do
   describe 'POST /authenticate' do
+    let(:user) { FactoryBot.create(:user, username: 'User1') }
     it 'authenticates the client' do
-      post '/api/v1/authenticate', params: { username: 'User1', password: 'password' }
+      post '/api/v1/authenticate', params: { username: user.username, password: 'password' }
       expect(response).to have_http_status(:created)
       expect(response_body).to eq({
                                     'token' => '123'
