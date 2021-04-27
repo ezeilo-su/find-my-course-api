afff38a4e774e2534d23e0f46a5f919204ff34b9
ezeilo-su <ezeilo.su@gmail.com>
2021-04-27 15:30:03 +0100

    Version user_registration. Add courses route.

---
+++ app/controllers/api/v1/user_registration_controller.rb
@@ -0,0 +1,31 @@
+module Api
+  module V1
+    class UserRegistrationController < ApplicationController
+      rescue_from ActionController::ParameterMissing, with: :parameter_missing
+    
+      def create
+        user = User.new(user_params)
+        if user.save
+            token = AuthenticationTokenService.call(user.id)
+            render json: {
+              token: token,
+              username: user.username
+            }, status: :created
+        else
+          render json: { error: user.errors.messages }, status: :unprocessable_entity
+        end
+      end
+    
+      private
+    
+      def user_params
+        params.require(:user).permit(:username, :password, :password_confirmation)
+      end
+      
+      def parameter_missing(err)
+        render json: { error: err.message }, status: :unprocessable_entity
+      end
+    end
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  post :signup, to: 'user_registration#create'
   
   namespace :api do
     namespace :v1 do
+      post :signup, to: 'user_registration#create'
       post 'authenticate', to: 'authentication#create'
+      resources :courses, only: [:index, :show], param: :slug
     end
   end
 end
