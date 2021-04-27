69bfbdf7faf2dd6680dba7c88bafbaf857199f24
ezeilo-su <ezeilo.su@gmail.com>
2021-04-27 18:01:47 +0100

    Use variable for token in the courses_spec.

--- spec/requests/courses_spec.rb
+++ spec/requests/courses_spec.rb
@@ -13,12 +13,22 @@ RSpec.describe 'Courses', type: :request do
       end
     end
 
+    let(:user) do
+      FactoryBot.create(
+        :user,
+        username: 'zeilotech',
+        password: 'password',
+        password_confirmation: 'password'
+      )
+    end
+
     it 'it returns all courses' do
+      token = AuthenticationTokenService.encode(user.id)
       get '/api/v1/courses',
           headers: {
-            'Authorization' => 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwidXNlcl9pZCI6IjEifQ.VV1SeoouiWqjlgs-3Z6zKBfNQ2K9TfajiBre_qJiOZQ'
+            'Authorization' => "Bearer #{token}"
           }
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(:ok)
       expect(response_body.size).to eq(2)
       expect(response_body['data'].size).to eq(5)
     end
