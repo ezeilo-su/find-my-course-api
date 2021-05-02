7566f73d8f0837a5a6c0c5062e0d0860fc7fbc3d
ezeilo-su <ezeilo.su@gmail.com>
2021-05-02 13:28:22 +0100

    Add a default_scope to favorite model.

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,6 +2,7 @@ class Course < ApplicationRecord
   has_many :reviews, dependent: :destroy
   before_save :slugify
 
+  default_scope -> { order(created_at: :desc) }
   def slugify
     self.slug = title.parameterize
   end
